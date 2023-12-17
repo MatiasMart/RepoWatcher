5829609c720243b590bb08146efdb6642696a113
Matias Martinelli <54157579+MatiasMart@users.noreply.github.com>
2023-12-17 12:22:53 -0300

    Initial Commit

---
+++ RepoWatcher/RepoWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoWatcherApp.swift
+//  RepoWatcher
+//
+//  Created by Matias Martinelli on 17/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct RepoWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
